19d3aacd8a177778e829f78eca7d9f33451d4d95
Ahmed149-m <idmachicheahmed59@gmail.com>
2026-08-06 15:21:07 +0100

    Update middleware public routes and matcher

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
+  '/',
+  '/search(.*)',
+  '/courses(.*)',
+  '/resources(.*)',
+  '/announcements(.*)',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/api/webhook(.*)'
+  '/api/webhook(.*)',
 ])
 
 export default clerkMiddleware(async (auth, req) => {
@@ -18,7 +23,5 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
   ],
 }
